9e8a60bcbba47e0c50c807350a8310ac40b576d1
Necroskillz <necroskillz@gmail.com>
2023-02-12 23:27:25 +0100

    feat: change cursor and disable drag on actions that cant be bound

--- src/features/actions/Action.tsx
+++ src/features/actions/Action.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React, { FC } from 'react';
 import { useDrag } from 'react-dnd';
 import { actions } from '../combat/actions';
@@ -22,8 +23,10 @@ export const Action: FC<ActionProps> = ({ action }) => {
       <ActionTooltip anchorId={`action_${action.id}`} action={action} combatAction={combatAction} />
       <div id={`action_${action.id}`} className="grid auto-cols-max grid-flow-col gap-2 items-center">
         {!combatAction && <div className="text-red-500 static text-4xl">?</div>}
-        <div ref={drag}>
-          <img className="w-10" src={'https://xivapi.com' + action.icon} alt={action.name} />
+        <div className={clsx({ 'cursor-not-allowed': !action.isAssignableToHotbar })}>
+          <div ref={drag} className={clsx({ 'pointer-events-none': !action.isAssignableToHotbar })}>
+            <img className="w-10" src={'https://xivapi.com' + action.icon} alt={action.name} />
+          </div>
         </div>
         <div>
           <div>{action.name}</div>
