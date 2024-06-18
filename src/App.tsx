7762372b2c779687211910df72b8370a333441ec
Necroskillz <necroskillz@gmail.com>
2024-06-18 21:59:44 +0200

    feat: make sunset page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import './App.css';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import { Hud } from './features/hud/Hud';
-import { ControlBar } from './features/hud/ControlBar';
 import { useEffect } from 'react';
 import { init } from './features/script_engine/scriptEngineSlice';
-import { useAppDispatch } from './app/hooks';
+import { useAppDispatch, useAppSelector } from './app/hooks';
 
 function App() {
   const dispatch = useAppDispatch();
+  const appState = useAppSelector((s) => s);
+
+  const data: any = {};
+
+  data.layout = {
+    elements: appState.hud.elements,
+    hotbars: appState.hotbars.hotbars.map((h) => ({ id: h.id, config: h.config })),
+  };
+
+  data.hotbarMapping = appState.hotbars.hotbars.map((h) => ({ id: h.id, slots: h.slots }));
+
+  data.keybindings = appState.hotbars.hotbars.map((h) => ({ id: h.id, keybinds: h.keybinds }));
+
+  data.scripts = appState.scriptEngine.scripts
+    .filter((s) => s.errors.length === 0)
+    .map((s) => ({ name: s.name, job: s.job, script: s.script }));
+
+  const exportData = JSON.stringify(data);
 
   useEffect(() => {
     const callback = (e: MouseEvent) => {
@@ -29,11 +45,19 @@ function App() {
   return (
     <div className="App">
       <DndProvider backend={HTML5Backend}>
-        <div className="p-2 min-h-screen grid grid-rows-[1fr_50px]">
-          <Hud />
-          <div className="self-end">
-            <ControlBar />
+        <div className="p-2 min-h-screen">
+          <div className="mb-8">
+            The site has moved to{' '}
+            <a className="text-xiv-orange" href="https://ffxiv-rotation-dojo.vercel.app/">
+              https://ffxiv-rotation-dojo.vercel.app
+            </a>
+            . You can copy your layout, keybindings and scripts configuration from the below field, and use import feature in the new app
+            location to restore it.
           </div>
+          <textarea
+            value={exportData}
+            className="w-[800px] h-[580px] text-black"
+          ></textarea>
         </div>
       </DndProvider>
     </div>
